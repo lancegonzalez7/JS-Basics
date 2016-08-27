4d7956b7a209bbcc607d392aa6db3c2f39f3ce03
Lance Gonzalez <Lance.gonzalez7@Gmail.com>
2016-08-27 10:39:16 -0600

    first edit

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -3,27 +3,30 @@
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
-
-  //Code Here
-
+function isTyler(name){
+  if (name==='Tyler') {
+    return true;
+  } else {
+    return false;
+  }
+}
 //////////////////PROBLEM 2////////////////////
 
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
-
-  //Code Here
-
+function getName() {
+  var userName = prompt("What is your name");
+  return userName;
+}
 
 //////////////////PROBLEM 3////////////////////
 
 
-
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
-
-  //Code Here
-
+function welcome() {
+  alert("Welcome " + getName());
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -32,7 +35,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+nothing  //Answer Here
 
 
 //////////////////PROBLEM 5////////////////////
@@ -42,7 +45,11 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+NaN
+false
+0
+' '
+null  //Answer Here
 
 
 
@@ -52,16 +59,18 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
-  
+function myName() {
+  return 'Lance'
+}  //Code Here
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
-
+var newMyName = myName();
   //Code Here
 
 //Now alert the result of invoking newMyName
-
+alert(newMyName());
 
 
 //////////////////PROBLEM 7////////////////////
